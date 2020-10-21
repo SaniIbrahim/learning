2e61466e62a88a6f288cf42485b7fd41f9b9ccab
SaniIbrahim <saniibrahimvws1@dmail.com>
2020-10-21 09:14:16 +0100

    E-learning first commition

---
+++ educa/educa/settings/pro.py
@@ -0,0 +1,19 @@
+from .base import *
+
+
+DEBUG = False
+
+
+ADMINS = (
+        ('Sani Ibrahim', 'saniibrahimvws1@gmail.com'),
+        )
+
+ALLOWED_HOSTS = ['*']
+
+
+DATABASES = {
+        'default': {
+
+            }
+
+        }
